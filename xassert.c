cee90fc5450bd3a58a6b57e270be02912e7c078f
cinsk <cinsk>
2004-11-05 00:12:09 +0000

    added: module xassert, enhanced assert(3)

---
+++ xassert.c
@@ -0,0 +1,36 @@
+/* $Id$ */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <xassert.h>
+
+
+void
+assert_(const char *file, long line, const char *func,
+        const char *expr, const char *format, ...)
+{
+  va_list ap;
+
+  fflush(stdout);
+  fflush(stderr);               /* possibly redundant */
+  fprintf(stderr, "%s:%ld: Assertion `%s' failed at %s.\n\t",
+          file, line, expr, func);
+  va_start(ap, format);
+  vfprintf(stderr, format, ap);
+  va_end(ap);
+  fputc('\n', stderr);
+  fflush(stderr);               /* possibly redundant */
+  abort();
+}
+
+
+#ifdef TEST_XASSERT
+int
+main(void)
+{
+  int age = 1;
+  ASSERT(age > 10, "Age should be larger than 10. (age = %d)", age);
+  return 0;
+}
+#endif /* TEST_XASSERT */
---
+++ xassert.h
@@ -0,0 +1,38 @@
+/* $Id$ */
+#ifndef XASSERT_H_
+#define XASSERT_H_
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+/* This indirect writing of extern "C" { ... } makes Emacs happy */
+#ifndef BEGIN_C_DECLS
+# define BEGIN_C_DECLS  extern "C" {
+# define END_C_DECLS    }
+#endif /* BEGIN_C_DECLS */
+
+#ifdef __cplusplus
+BEGIN_C_DECLS
+#endif
+
+
+#ifndef NDEBUG
+extern void assert_(const char *file, long line, const char *func,
+                    const char *expr, const char *format, ...);
+# define ASSERT(condition, ...) do {                                          \
+                                  if (!(condition))                           \
+                                    assert_(__FILE__, __LINE__, __func__,     \
+                                            #condition, __VA_ARGS__);         \
+                                } while (0)
+
+#else
+# define ASSERT(condition, ...) ((void)0)
+#endif /* NDEBUG */
+
+
+#ifdef __cplusplus
+END_C_DECLS
+#endif
+
+#endif /* XASSERT_H_ */

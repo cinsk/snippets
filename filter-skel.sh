4fb9ff91635df48b278de452fee858ecfbacf7ae
cinsk <cinsk>
2005-07-13 01:53:49 +0000

    added: skeleton filter program using sh.

---
+++ filter-skel.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+# -*-sh-mode-*-
+# $Id$
+
+PATH=/usr/local/bin:/usr/bin:/bin
+
+program_name='skel'
+version_string='0.1'
+
+output_filename=
+verbose_mode=1
+
+
+function error () {
+    # usage: error EXIT_STATUS MESSAGE...
+    # Print MESSAGE to the standard error (stderr) and call `exit EXIT_STATUS'
+    # if EXIT-STATUS is nonzero.
+    status="$1"
+    shift;
+    test -n "$program_name" && echo -n "$program_name: "
+    echo "$@"
+    test 0 -ne "$status" && exit "$status";
+} 1>&2
+
+
+function help_and_exit() {
+    cat <<EOF
+usage: $program_name [OPTION...] FILE...
+
+  -q        quite mode
+  -o FILE   send output to file FILE. If FILE is \`-',
+           send output to stdout.
+
+  -h        display this help and exit
+  -v        output version information and exit
+
+Report bugs to <cinsky at gmail dot com>
+
+EOF
+    exit 0
+}
+
+
+function version_and_exit() {
+    cat <<EOF
+$program_name version $version_string
+EOF
+    exit 0
+}
+
+
+# The actual code begins here -------------
+
+while getopts "hvo:q" opt; do
+    case $opt in
+        '?')
+            error 1 "Try \`-h' for more information"
+            ;;
+        'h')
+            help_and_exit
+            ;;
+        'v')
+            version_and_exit
+            ;;
+        'o')
+            output_filename=$OPTARG;
+            ;;
+        'q')
+            verbose_mode=0
+            ;;
+        *)
+            error 1 "Internal getopts error"
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+echo "output_filename: $output_filename"
+echo "verbose_mode: $verbose_mode"
+
+for f in "$@"; do
+    echo "arg = $f"
+done
---
+++ filter-skel2.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# -*-sh-mode-*-
+# $Id$
+
+PATH=/usr/local/bin:/usr/bin:/bin
+
+program_name='skel2'
+version_string='0.1'
+
+output_filename=
+verbose_mode=1
+
+function error() {
+    # usage: error EXIT_STATUS MESSAGE...
+    # Print MESSAGE to the standard error (stderr) and call `exit EXIT_STATUS'
+    # if EXIT-STATUS is nonzero.
+    status="$1"
+    shift;
+    test -n "$program_name" && echo -n "$program_name: "
+    echo "$@"
+    test 0 -ne "$status" && exit "$status";
+} 1>&2
+
+function help_and_exit() {
+    cat <<EOF
+usage: $program_name [OPTION...] FILE...
+
+  -q, --quiet        quite mode
+  -o, --output=FILE  send output to file FILE. If FILE is \`-',
+                     send output to stdout.
+
+      --help         display this help and exit
+      --version      output version information and exit
+
+Report bugs to <cinsky at gmail dot com>
+
+EOF
+    exit 0
+}
+
+function version_and_exit() {
+    cat <<EOF
+$program_name version $version_string
+EOF
+    exit 0
+}
+
+
+# The actual code begins here -------------
+
+GETOPT=`getopt -o o:q --long help,version,quiet,output: -n "$program_name" -- "$@"`
+if [ $? -ne 0 ]; then
+    error 1 "Try \`$program_name --help' for more information"
+fi
+
+eval set -- "$GETOPT"
+
+while true; do
+    case "$1" in
+        --help)
+            help_and_exit
+            shift
+            ;;
+        --version)
+            version_and_exit
+            shift
+            ;;
+        -o|--output)
+            output_filename="$2"
+            shift 2
+            ;;
+        -q|--quiet)
+            verbose_mode=0
+            shift;
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+echo "output_filename: $output_filename"
+echo "verbose_mode: $verbose_mode"
+
+for f in "$@"; do
+    echo "arg = $f"
+done

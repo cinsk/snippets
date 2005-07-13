f1ea6d924df8e53b072cbbe2c274b61835c04e79
cinsk <cinsk>
2005-07-13 04:37:03 +0000

    added: copyright notice

--- filter-skel.sh
+++ filter-skel.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 # -*-sh-mode-*-
+
+#
+# Skeleton Shell script using built-in getopts
+# Copyright (C) 2005  Seong-Kook Shin <cinsky at gmail dot com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 # $Id$
 
 PATH=/usr/local/bin:/usr/bin:/bin

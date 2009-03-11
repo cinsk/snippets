c21bf1e424ec93e91fa6b04e5b00c430304168d9
cinsk <cinsk>
2009-03-11 02:00:30 +0000

    fixed: removed redundant `;' character

--- easyconv.h
+++ easyconv.h
@@ -27,7 +27,7 @@
 #ifndef BEGIN_C_DECLS
 # ifdef __cplusplus
 #  define BEGIN_C_DECLS extern "C" {
-#  define END_C_DECLS   };
+#  define END_C_DECLS   }
 # else
 #  define BEGIN_C_DECLS
 #  define END_C_DECLS

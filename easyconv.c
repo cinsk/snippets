da7718d7e98cec6a7448876a5078c19df17d7b0c
cinsk <cinsk>
2009-03-11 01:58:31 +0000

    added: easyconv module

---
+++ easyconv.c
@@ -0,0 +1,173 @@
+/* $Id$ */
+/* easy interface for GNU iconv module.
+ * Copyright (C) 2009  Seong-Kook Shin <cinsky@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "easyconv.h"
+
+#ifdef EASYCONV_VERBOSE
+#include <stdio.h>
+#include <ctype.h>
+#endif
+
+size_t
+easyconv_str2str(iconv_t CD, char **DST, size_t *DST_SIZE,
+                 const char *SRC, size_t SRC_SIZE, size_t *NREVCONV)
+{
+  char *dst, *src;
+  size_t dsize, ssize, conv;
+  int i, olderr = 0;
+
+  assert(DST != NULL);
+  assert(DST_SIZE != NULL);
+
+  if (SRC_SIZE == (size_t)-1)
+    SRC_SIZE = strlen(SRC);
+
+  if (*DST == NULL) {
+    int len = (*DST_SIZE) ? *DST_SIZE : EASYCONV_CHUNK_SIZE;
+    *DST = malloc(len);
+    if (!*DST)
+      return (size_t)-1;
+    //memset(*DST, 0xCC, len);
+    *DST_SIZE = len;
+  }
+
+ retry:
+  dst = *DST;
+  dsize = *DST_SIZE;
+  src = (char *)SRC;
+  ssize = SRC_SIZE;
+
+  iconv(CD, NULL, NULL, &src, &ssize);
+
+  errno = 0;
+  conv = iconv(CD, &src, &ssize, &dst, &dsize);
+  if (NREVCONV)
+    *NREVCONV = conv;
+  olderr = errno;
+
+  if (conv == (size_t)-1) {
+    if (errno == E2BIG) {
+      int len;
+      if (*DST_SIZE < EASYCONV_CHUNK_SIZE)
+        len = EASYCONV_CHUNK_SIZE;
+      else
+        len = (*DST_SIZE) * 2;
+
+      *DST = realloc(*DST, len);
+      if (!*DST)
+        return (size_t)-1;
+      *DST_SIZE = len;
+      goto retry;
+    }
+    else if (errno == EILSEQ) {
+#ifdef EASYCONV_VERBOSE
+      int i;
+      int len = (ssize > 16) ? 16 : ssize;
+
+      fprintf(stderr, "error: iconv failed(EILSEQ): ");
+      for (i = 0; i < len; i++) {
+        unsigned char ch = src[i];
+        if (isprint(ch))
+          fprintf(stderr, "%c", ch);
+        else
+          fprintf(stderr, "\\x%02X", ch);
+      }
+      fputc('\n', stderr);
+#endif  /* EASYCONV_VERBOSE */
+    }
+    else if (errno != EINVAL)
+      return (size_t)-1;
+
+    /* If you want to get the error report of EILSEQ, return
+     * (size_t)-1 here.  If you do, the caller may not use the
+     * contents in *DST since it does not end with the EOL. */
+  }
+
+  if (dsize < 4) {
+    /* We don't have enough space for the EOL. */
+    int len = *DST_SIZE + 4;
+    int off = dst - *DST;
+    *DST = realloc(*DST, len);
+    if (!*DST)
+      return (size_t)-1;
+    *DST_SIZE = len;
+    dst = *DST + off;
+  }
+
+  /* Terminate it with the EOL. */
+  for (i = 0; i < 4; i++)
+    *dst++ = '\0';
+
+  errno = olderr;
+
+  return dst - *DST;
+}
+
+
+#ifdef TEST_EASYCONV
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+  iconv_t cd;
+  char *converted;
+  size_t size, nrevconv, ret;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s ENCODING TEXT\n", argv[0]);
+    return 1;
+  }
+
+  cd = iconv_open(argv[1], "UTF-8");
+  if (cd == (iconv_t)-1) {
+    fprintf(stderr, "error: iconv_open() failed: %s", strerror(errno));
+    return 1;
+  }
+
+#if 0
+  converted = NULL;
+  size = 0;
+#else
+  converted = malloc(1);
+  *converted = 1;
+  size = 1;
+#endif
+
+  ret = easyconv_str2str(cd, &converted, &size, argv[2], (size_t)-1, &nrevconv);
+  if (ret != (size_t)-1) {
+    fprintf(stderr, "%d byte(s) converted\n", ret);
+    fprintf(stderr, "%d character(s) converted\n", nrevconv);
+    write(STDOUT_FILENO, converted, ret);
+  }
+  else
+    fprintf(stderr, "error: easyconv_str2str failed: %s\n", strerror(errno));
+  free(converted);
+
+  iconv_close(cd);
+
+  return 0;
+}
+#endif  /* TEST_EASYCONV */
---
+++ easyconv.h
@@ -0,0 +1,109 @@
+/* $Id */
+/* easy interface for GNU iconv module.
+ * Copyright (C) 2009  Seong-Kook Shin <cinsky@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EASYCONV_H__
+#define EASYCONV_H__
+
+#include <stddef.h>
+#include <iconv.h>
+
+/* This indirect using of extern "C" { ... } makes Emacs happy */
+#ifndef BEGIN_C_DECLS
+# ifdef __cplusplus
+#  define BEGIN_C_DECLS extern "C" {
+#  define END_C_DECLS   };
+# else
+#  define BEGIN_C_DECLS
+#  define END_C_DECLS
+# endif
+#endif /* BEGIN_C_DECLS */
+
+BEGIN_C_DECLS
+
+/*
+ * If EASYCONV_VERBOSE is defined, most easyconv functions print
+ * error/warning messages into stderr.
+ */
+#define EASYCONV_VERBOSE
+
+/*
+ * EASYCONV_CHUNK_SIZE is the default size of memory chunk of the DST.
+ * If the memory chunk is too small, it will be increased by doubling.
+ */
+#define EASYCONV_CHUNK_SIZE     256
+
+
+/*
+ * easyconv_str2str(): character set conversion from the source string
+ * to another string.
+ *
+ * easyconv_str2str() convert the character set of bytes in `SRC'
+ * buffer with the size `SRC_SIZE' using iconv module `CD'.  The
+ * successfully converted characters are stored in `*DST'.
+ *
+ * easyconv_str2str() dynamically allocated the storage for the
+ * destination string.  Before calling this function, you should place
+ * in `*DST' the address of a buffer `*DST_SIZE' bytes long, allocated
+ * with `malloc'.  If this buffer is not enough for the conversion,
+ * this function make the buffer bigger using `realloc()', storing the
+ * new buffer address back in `*DST' and the increased size back in
+ * `*DST_SIZE'.
+ *
+ * If you set `*LINEPTR' to a null pointer and `*DST_SIZE' to zero,
+ * this function will allocates the initial buffer for you by calling
+ * `malloc()'.  The default buffer size is controllable via
+ * EASYCONV_CHUNK_SIZE.
+ *
+ * If you set `*LINEPTR' to a null pointer with nonzero `*DST_SIZE',
+ * this function will allocate the initial buffer with the size in
+ * `*DST_SIZE'.
+ *
+ * In any case (even if easyconv_str2str reports an error), you must
+ * call `free()' with `*DST' if the storage is no longer required.
+ *
+ * easyconv_str2str returns the size of the successfully converted
+ * bytes with cleared `errno'.  Note that the return value is not the
+ * same as that of `iconv()'.  The return value of internal `iconv()'
+ * will be stored in `*NREVCONV' if `NREVCONV' is not NULL.
+ *
+ * There are two error conditions; one for that it cannot handle, and
+ * one for that easyconv_str2str() can handle.  In the former case,
+ * easyconv_str2str() returns (size_t)-1 with the appropriate error in
+ * `errno'.  In case of the latter, easyconv_str2str() returns the
+ * successfully converted bytes, with the appropriate code in `errno'.
+ *
+ * easyconv_str2str silently handles EINVAL and EILSEQ of iconv().
+ *
+ * EINVAL: easyconv_str2str() will truncate any incomplete bytes
+ * sequence at the input buffer.  Thus, it is recommended to pass a
+ * complete byte sequence in SRC.
+ *
+ * EILSEQ: easyconv_str2str() will truncate any invalid byte sequence
+ * in the input, then returns only the first valid converted byte
+ * sequence.
+ *
+ * For the example using this function, see the main() in <easyconv.c>
+ */
+extern size_t easyconv_str2str(iconv_t CD, char **DST, size_t *DST_SIZE,
+                               const char *SRC, size_t SRC_SIZE,
+                               size_t *NREVCONV);
+
+END_C_DECLS
+
+#endif  /* EASYCONV_H__ */

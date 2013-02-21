e0568d0734000ba3ad3733fc5236f830af2a5550
cinsk@selune <cinsky@gmail.com>
2013-02-21 16:03:18 +0900

    add backtrace utility on xerror

--- xerror.h
+++ xerror.h
@@ -34,9 +34,19 @@ BEGIN_C_DECLS
 extern void xerror(int status, int code, const char *format, ...)
   __attribute__((format (printf, 3, 4)));
 
+/*
+ * xdebug(...) is like xerror(0, ...), except that it embeds the
+ * caller's filename and line number in the messages.  The output will
+ * not be generated if the application defined 'debug_mode' to zero.
+ *
+ * By default, 'debug_mode' is set to zero.
+ */
 #define xdebug(code, fmt, ...)                                          \
     xdebug_((code), ("%s:%d: " fmt), __FILE__, __LINE__, ## __VA_ARGS__)
 
+/*
+ * Return nonzero if 'debug_mode' is nonzero.
+ */
 extern int xifdebug(void);
 
 extern void xdebug_(int code, const char *format, ...)
@@ -44,7 +54,23 @@ extern void xdebug_(int code, const char *format, ...)
 
 extern void xmessage(int progname, int code, const char *format, va_list ap);
 
+/*
+ * By default, all x*() functions will send the output to STDERR.
+ * You can override the output stream using xerror_redirect().
+ *
+ * This function returns the previous output stream if any.
+ *
+ * Note that if you didn't set explicitly the output stream to STDERR,
+ * this function will return NULL.  This may be helpful if you want to
+ * close the previous output stream except STDERR.
+ */
 extern FILE *xerror_redirect(FILE *fp);
+
+/*
+ * Register one or more signals to generate backtrace if the program
+ * receives signals.  Note that the last argument should be zero.
+ */
+extern int xbacktrace_on_signals(int signo, ...);
 
 END_C_DECLS
 

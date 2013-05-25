0f359c94cf0be9b2c839c31824674a5d6d9cd2f6
cinsk@torm <cinsky@gmail.com>
2013-05-24 18:03:33 -0700

    add benchmark on `jsonc-sample.c`

--- jsonc-sample.c
+++ jsonc-sample.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <json/json.h>
-
+#include "difftime.h"
 
 struct json_object *json_object_object_xget(struct json_object *obj,
                                             /* keys */ ...);
@@ -12,6 +12,8 @@ struct json_object *json_object_object_getv(struct json_object *obj,
                                             const char *keys[],
                                             size_t nkeys);
 
+
+#define BUF_MAX 4096
 
 int
 main(int argc, char *argv[])
@@ -26,37 +28,70 @@ main(int argc, char *argv[])
   struct json_object *json1;
   struct json_object *json2;
   struct json_object *json3;
+  int i;
+  long nsec;
+  char src1[BUF_MAX], src2[BUF_MAX];
 
-  json1 = json_object_from_file(argv[1]);
-  json2 = json_object_from_file(argv[2]);
+  {
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    if (!fp) {
+      fprintf(stderr, "error: cannot open %s\n", argv[1]);
+      return 1;
+    }
+    fread(src1, 1, BUF_MAX, fp);
+    fclose(fp);
 
-  json3 = json_object_new_object();
-  //json_object_array_add(json3, json1);
-  //json_object_array_add(json3, json2);
+    fp = fopen(argv[2], "r");
+    if (!fp) {
+      fprintf(stderr, "error: cannot open %s\n", argv[2]);
+      return 1;
+    }
+    fread(src2, 1, BUF_MAX, fp);
+    fclose(fp);
+  }
+
+  DIFFTIME(nsec) {
+
+    for (i = 0; i < 10000; i++) {
+      json1 = json_tokener_parse(src1);
+      json2 = json_tokener_parse(src2);
+      json3 = json_object_new_object();
+      //json_object_array_add(json3, json1);
+      //json_object_array_add(json3, json2);
+      //json_object_to_file("/dev/stdout", json3);
+
+      {
+        json_object_object_foreach(json1, key, val) {
+          //fprintf(stderr, "adding %s\n", key);
+          json_object_object_add(json3, key, val);
+          json_object_get(val);
+        }
+      }
+
+      {
+        json_object_object_foreach(json2, key, val) {
+          //fprintf(stderr, "adding %s\n", key);
+          json_object_object_add(json3, key, val);
+          json_object_get(val);
+        }
+      }
+#if 0
+      if (i == 10000 - 1)
+        json_object_to_file("/dev/stdout", json3);
+#endif  /* 0 */
+
+      json_object_put(json3);
+      json_object_put(json1);
+      json_object_put(json2);
+    }
+
+  }
+  printf("%ld\n", nsec);
+  printf("%ld\n", nsec / 10000);
 
   //json_object_to_file("/dev/stdout", json3);
 
-  {
-    json_object_object_foreach(json1, key, val) {
-      fprintf(stderr, "adding %s\n", key);
-      json_object_object_add(json3, key, val);
-      json_object_get(val);
-    }
-  }
-
-  {
-    json_object_object_foreach(json2, key, val) {
-      fprintf(stderr, "adding %s\n", key);
-      json_object_object_add(json3, key, val);
-      json_object_get(val);
-    }
-  }
-
-  json_object_to_file("/dev/stdout", json3);
-
-  json_object_put(json1);
-  json_object_put(json2);
-  json_object_put(json3);
 
   return 0;
 }
